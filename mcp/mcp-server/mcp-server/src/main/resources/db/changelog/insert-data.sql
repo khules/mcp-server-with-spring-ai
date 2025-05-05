dfd980b5052fa84747e4948f7f27cf85df78cca5
kuldeep Singh <kuldeepsingh99@gmail.com>
2025-05-05 21:42:50 +0200

    Added Liquibase change

---
+++ mcp/mcp-server/mcp-server/src/main/resources/db/changelog/insert-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO seller_account (id, "name", "owner", is_test, "type", status) VALUES(1, 'David', 'amazon', true, 'STD', 1);
+INSERT INTO seller_account (id, "name", "owner", is_test, "type", status) VALUES(2, 'Marc', 'flipkart', true, 'STD', 1);
+INSERT INTO seller_account (id, "name", "owner", is_test, "type", status) VALUES(3, 'Mike', 'amazon', true, 'STD', 1);
+INSERT INTO seller_account (id, "name", "owner", is_test, "type", status) VALUES(4, 'Paul', 'dell', true, 'STD', 1);
